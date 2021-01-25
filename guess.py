f78c44064520f7cd95fa57366d0f53d6d4e213de
BaconSu <nick92816@gmail.com>
2021-01-25 14:45:07 +0800

    add a new file

--- guess.py
+++ guess.py
@@ -3,13 +3,18 @@ import random
 r = random.randint(1,100)
 print(r)
 
+count = 0
+
 while True:
 	num = input('請輸入數字:')
 	num = int(num)
+	count += 1
 	if num == r:
- 		print('你猜中了!')
- 		break
+		print('你猜中了!')
+		print('你總共猜了', count, '次')
+		break
 	elif num > r:
- 		print('比答案大')
+		print('比答案大')
 	elif num < r:
- 		print ('比答案小') 
+		print ('比答案小') 
+	print('你已經猜了', count, '次')
